05450579f6c1b3315d9ab4afaf8cfb8276e9f402
Victoria <kamorinavika@gmail.com>
2021-05-14 22:08:20 +0300

    Add Board endpoints with separate router paths.

---
+++ src/resources/boards/board.router.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const Board = require('./board.model');
+const boardsService = require('./board.service');
+
+router.route('/').get(async (req, res) => {
+  const boards = await boardsService.getAll();
+  res.json(boards.map(Board.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  try {
+    const board = await boardsService.get(req.params.id);
+    res.json(Board.toResponse(board));
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  const board = await boardsService.create(
+    new Board({
+      title: req.body.title,
+      columns: req.body.columns
+    })
+  );
+
+  res.json(Board.toResponse(board));
+});
+
+router.route('/:id').put(async (req, res) => {
+  try {
+    const board = await boardsService.update(req.params.id,
+      {
+        title: req.body.title,
+        columns: req.body.columns
+      });
+
+      res.json(Board.toResponse(board));
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+router.route('/:id').delete(async (req, res) => {
+  try {
+    const board = await boardsService.remove(req.params.id);
+    res.json(Board.toResponse(board));
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+module.exports = router;
