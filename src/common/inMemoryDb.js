1deedb6c98a34029b6a042eefbe915a5c2c96386
Victoria <kamorinavika@gmail.com>
2021-05-15 14:28:33 +0300

    Add tasks' methods to DB; add extra methods.

--- src/common/inMemoryDb.js
+++ src/common/inMemoryDb.js
@@ -1,13 +1,33 @@
 const User = require('../resources/users/user.model');
 const Board = require('../resources/boards/board.model');
+const Task = require('../resources/tasks/task.model');
 
 const DB = {
   Users: [],
   Boards: [],
-  Tasks: []
+  Tasks: [],
+  removeTasksOfRemovedBoard: board => {
+    if (board) {
+      DB.Tasks.filter(task => task && task.boardId === board.id).forEach(
+        // eslint-disable-next-line no-return-assign
+        task => (DB.Tasks.splice(DB.Tasks.indexOf(task),1))
+      );
+    }
+  },
+  updateTaskForDeletedUser: user => {
+    if (user) {
+      DB.Tasks.filter(task => task).forEach(task => {
+        // eslint-disable-next-line no-param-reassign
+        task.userId = task.userId === user.id ? null : task.userId;
+      });
+    }
+  }
 }
 
-DB.Users.push(new User(), new User(), new User());
+const newUser1 = new User();
+const newUser2 = new User();
+const newUser3 = new User();
+DB.Users.push(newUser1, newUser2, newUser3);
 
 const getAllUsers = async () => DB.Users.slice(0);
 
@@ -29,12 +49,14 @@ const updateUser = async (id, user) => {
 
 const removeUser = async id => {
   const user = DB.Users.filter(el => el.id === id)[0];
+  DB.updateTaskForDeletedUser(user);
   const userIndex = DB.Users.indexOf(user);
   DB.Users.splice(userIndex,1);
   return user;
 }
 
-DB.Boards.push(new Board(), new Board(), new Board());
+const newBoard = new Board();
+DB.Boards.push(newBoard);
 
 const getAllBoards = async () => DB.Boards.slice(0);
 
@@ -55,9 +77,46 @@ const updateBoard = async (id, board) => {
 
 const removeBoard = async id => {
   const board = DB.Boards.filter(el => el.id === id)[0];
+  DB.removeTasksOfRemovedBoard(board);
   const boardIndex = DB.Boards.indexOf(board);
   DB.Boards.splice(boardIndex,1);
   return board;
+}
+
+DB.Tasks.push(
+  new Task({ boardId: newBoard.id, userId: newUser1.id }),
+  new Task({ boardId: newBoard.id, userId: newUser2.id })
+);
+
+const getAllTasks = async boardId => DB.Tasks.filter(el => el.boardId === boardId);
+
+const getTask = async (boardId, taskId) =>
+  DB.Tasks
+  .filter(el => el.boardId === boardId)
+  .filter(task => task.id === taskId)[0];
+
+const createTask = async (boardId, task) => {
+  DB.Tasks.push(task);
+  return task;
+};
+
+const updateTask = async (boardId, taskId, task) => {
+  const taskOld = DB.Tasks.filter(el => el.boardId === boardId).filter(elTask => elTask.id === taskId)[0];
+  const taskIndex = DB.Tasks.indexOf(taskOld);
+  DB.Tasks[taskIndex].title = task.title;
+  DB.Tasks[taskIndex].order = task.order;
+  DB.Tasks[taskIndex].description = task.description;
+  DB.Tasks[taskIndex].userId = task.userId;
+  DB.Tasks[taskIndex].boardId = task.boardId;
+  DB.Tasks[taskIndex].columnId = task.columnId;
+  return DB.Tasks[taskIndex];
+}
+
+const removeTask = async (boardId, taskId) => {
+  const task = DB.Tasks.filter(el => el.boardId === boardId).filter(elTask => elTask.id === taskId)[0];
+  const taskIndex = DB.Tasks.indexOf(task);
+  DB.Tasks.splice(taskIndex,1);
+  return task;
 }
 
 module.exports = {
@@ -70,5 +129,10 @@ module.exports = {
   getBoard,
   createBoard,
   updateBoard,
-  removeBoard
+  removeBoard,
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  removeTask
 };
