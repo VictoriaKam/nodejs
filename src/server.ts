e7cfa0f70cf3945858369ca5417ab203bfb1e1fd
Victoria <kamorinavika@gmail.com>
2021-06-20 17:33:47 +0300

    Add functionality that establish connection with DB.

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import config from './common/config';
 
 import * as app from './app';
 
+import { TryDBConnect } from '../src/helpers/db'
+
 const {PORT} = config;
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+TryDBConnect(() => {
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+});
