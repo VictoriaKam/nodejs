7768cb10e7cf0a55936e595fd5c5123558e29afc
Victoria <kamorinavika@gmail.com>
2021-06-20 17:40:36 +0300

    Update service layer for user, board and task.

--- src/resources/boards/board.service.ts
+++ src/resources/boards/board.service.ts
@@ -1,41 +1,15 @@
 import * as boardsRepo from './board.memory.repository';
+import { Board } from '../../entities/board.model'
 
-import {IBoard} from '../../types/interfaces';
+const getAll = (): Promise<Array<Board>> => boardsRepo.getAll();
 
-/**
- * Returns all existing boards.
- * @returns {Array.<object>} all boards.
- */
-const getAll = (): Promise<Array<IBoard>> => boardsRepo.getAll();
+const get = (id: string): Promise<Board> => boardsRepo.get(id);
 
-/**
- * Retrieves a board by id.
- * @param {String} id - Board id
- * @returns {Board} Board object.
- */
-const get = (id: string): Promise<IBoard> => boardsRepo.get(id);
+const create = (board: Board): Promise<Board> => boardsRepo.create(board);
 
-/**
- * Inserts a new board into boards repo.
- * @param {Board.<string>} board object.
- * @returns {Board.<string>} newly created Board object.
- */
-const create = (board: IBoard): Promise<IBoard> => boardsRepo.create(board);
+const update = async (id: string, board: Board): Promise<Board> => boardsRepo.update(id, board);
 
-/**
- * Updates a board with a specific id.
- * @param {String} id - Board id
- * @param {Board} board - Board object with new values.
- * @returns {Board} updated Board object.
- */
-const update = async (id: string, board: IBoard): Promise<IBoard> => boardsRepo.update(id, board);
-
-/**
- * Deletes a board with a specific id.
- * @param {String} id - Board id
- * @returns {Board} Board object that was removed.
- */
-const remove = (id: string): Promise<IBoard> => boardsRepo.remove(id);
+const remove = (id: string): Promise<string> => boardsRepo.remove(id);
 
 const _ = {
     getAll,
--- src/resources/tasks/task.service.ts
+++ src/resources/tasks/task.service.ts
@@ -1,46 +1,16 @@
 import * as tasksRepo from './task.memory.repository';
 
-import {ITask} from '../../types/interfaces';
+import { Task } from '../../entities/task.model'
 
-/**
- * Returns all existing tasks in particular board.
- * @param {String} boardId - Board id
- * @returns {Array.<object>} all tasks in particular board.
- */
-const getAll = (boardId: string): Promise<Array<ITask>> => tasksRepo.getAll(boardId);
+const getAll = (boardId: string): Promise<Array<Task>> => tasksRepo.getAll(boardId);
 
-/**
- * Retrieves a task by board id and task id.
- * @param {String} boardId - Board id
- * @param {String} taskId - Task id
- * @returns {Task} Task object.
- */
-const get = (boardId: string, taskId: string): Promise<ITask> => tasksRepo.get(boardId, taskId);
+const get = (boardId: string, id: string): Promise<Task> => tasksRepo.get(boardId, id);
 
-/**
- * Inserts a new task into particular board into tasks repo.
- * @param {String} boardId - Board id
- * @param {Task.<string>} Task object.
- * @returns {Task.<string>} newly created Task object.
- */
-const create = (boardId: string, task: ITask): Promise<ITask> => tasksRepo.create(boardId, task);
+const create = (boardId: string, task: Task): Promise<Task> => tasksRepo.create(boardId, task);
 
-/**
- * Updates a task with a specific id in a particular board.
- * @param {String} boardId - Board id
- * @param {String} taskId - Task id
- * @param {Task} Task - Taskd object with new values.
- * @returns {Task} updated Task object.
- */
-const update = (boardId: string, taskId: string, task: ITask): Promise<ITask> => tasksRepo.update(boardId, taskId, task);
+const update = (boardId: string, id: string, task: Task): Promise<Task> => tasksRepo.update(boardId, id, task);
 
-/**
- * Deletes a task with a specific id in a particular board.
- * @param {String} boardId - Board id
- * @param {String} taskId - Task id
- * @returns {Task} Task object that was removed.
- */
-const remove = (boardId: string, taskId: string): Promise<ITask> => tasksRepo.remove(boardId, taskId);
+const remove = (boardId: string, id: string): Promise<string> => tasksRepo.remove(boardId, id);
 
 const _ = {
     getAll,
