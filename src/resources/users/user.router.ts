276792ad394eb471be4845fe53180982aa239908
Victoria <kamorinavika@gmail.com>
2021-06-26 20:28:32 +0300

    Hide password field in res.

--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from 'express';
 import express = require('express');
 import * as usersService from './user.service';
+import { User } from '../../entities/user.model';
 
 const router = express.Router();
 
 router.route('/').get(async (_req: Request, res: Response): Promise<void> => {
   const users = await usersService.getAll();
-  if (users) { res.send(users) };
+  if (users) { res.send(users.map(User.toResponse)) };
 });
 
 router.route('/:id').get(async (req: Request, res: Response): Promise<void> => {
   const {id} = req.params;
   try {
     const user = await usersService.get(id);
-    res.json(user);
+    res.json(User.toResponse(user));
   } catch (e) {
     res.status(404).send(e.message);
   }
@@ -21,15 +22,14 @@ router.route('/:id').get(async (req: Request, res: Response): Promise<void> => {
 
 router.route('/').post(async (req: Request, res: Response): Promise<void> => {
   const user = await usersService.create(req.body);
-  const userToResponse = { id: user.id, login: user.login, name: user.name };
   res.status(201);
-  res.send(userToResponse);
+  res.send(User.toResponse(user));
 });
 
 router.route('/:id').put(async (req: Request, res: Response): Promise<void> => {
   try {
     const user = await usersService.update(req.params.id, req.body);
-    res.json(user);
+    res.json(User.toResponse(user));
   } catch (e) {
     res.status(404).send(e.message);
   }
